b36b7470125c47d1fcbc847987f5e9ee00d13474
unknown <Cetecom@SJAPCA08LC0707.duoc.acad>
2025-05-22 20:12:24 -0400

    Mostrar api

--- projectferremas/ferremas/urls.py
+++ projectferremas/ferremas/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProductosViewSet
+from .views import ProductosViewSet, productos_page
 
 router = DefaultRouter()
 router.register(r'ferremas', ProductosViewSet)
 
 urlpatterns = [
+    path('ferremas/', productos_page, name='productos-page'),
     path('', include(router.urls)),
+
 ]
--- projectferremas/ferremas/views.py
+++ projectferremas/ferremas/views.py
@@ -2,21 +2,12 @@
 from rest_framework import viewsets
 from .models import Productos
 from .serializers import ProductoSerializers
-import requests
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from django.shortcuts import render
 
 class ProductosViewSet(viewsets.ModelViewSet):
     queryset = Productos.objects.all()
     serializer_class = ProductoSerializers
 
 
-class PostsExternosView(APIView):
-    def get(self, request):
-        try:
-            response = requests.get('https://jsonplaceholder.typicode.com/posts')
-            data = response.json()
-            return Response(data, status=status.HTTP_200_OK)
-        except requests.exceptions.RequestException as e:
-            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+def productos_page(request):
+    return render(request, 'ferremas/home.html')
